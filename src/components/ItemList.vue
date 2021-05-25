5521546ebd7a11f39dd3c04f2d4b1dc3640243b8
spark-s-s <spark.h.sasaki@gmail.com>
2021-05-26 00:27:44 +0900

    list 2

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>ItemList</div>
+  <div>
+    <div>ItemList</div>
+    <div v-for="item in items" :key="item.name">
+      <div class="item">
+        <div class="name">名前: {{ item.name }}</div>
+        <div class="price">{{ item.price }} 円</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
-  name: "ItemList",
+  setup() {
+    const items = ref([
+      { name: "たまご", price: 100 },
+      { name: "りんご", price: 160 },
+    ]);
+    return { items };
+  },
 };
 </script>
 
