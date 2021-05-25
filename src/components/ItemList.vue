c622432c18cab8723153f999f08f34755c72b44b
spark-s-s <spark.h.sasaki@gmail.com>
2021-05-26 00:35:03 +0900

    list last

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -8,13 +8,6 @@
         <div v-if="item.price >= 10000">高額商品</div>
       </div>
     </div>
-    <div>ItemList</div>
-    <div v-for="item in items" :key="item.name">
-      <div class="item" :class="{ over500: item.price >= 500 }">
-        <div class="name">名前: {{ item.name }}</div>
-        <div class="price">{{ item.price }} 円</div>
-      </div>
-    </div>
     <div>
       <label>
         名前
