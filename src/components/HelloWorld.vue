2c51c8dca1db9a56fa1ebb00c4e15aab8ec2f1d9
spark-s-s <spark.h.sasaki@gmail.com>
2021-05-26 00:25:37 +0900

    list new

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,21 +2,24 @@
   <div>
     <h1>{{ msg }}</h1>
     <counter />
+    <item-list />
   </div>
 </template>
 
 <script>
 import Counter from "./Counter.vue";
+import ItemList from "./ItemList.vue";
 
 export default {
   components: {
     Counter,
+    ItemList,
   },
   props: {
     msg: {
       type: String,
       default: "",
-    }
+    },
   },
 };
 </script>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>ItemList</div>
+</template>
+
+<script>
+export default {
+  name: "ItemList",
+};
+</script>
+
+<style></style>
